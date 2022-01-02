60cb636f9bc7e2b0da8f3baa30946ef0570c49c4
Talic <talic.zealot@gmail.com>
2022-01-02 14:31:43 +0200

    delete config file

--- SotnRandoTools/src/Khaos/TwitchConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SotnRandoTools.Khaos
-{
-	public static class TwitchConfiguration
-	{
-		public static readonly string TwitchClientId = "<your twitch application client id>";
-		public static readonly string TwitchClientSecret = "<your twitch application client secret>";
-	}
-}
